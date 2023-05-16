b73e195c003cd175f2a6a0f7f6c7204fa979b73d
Mobin Arshad <mobink09@gmail.com>
2023-05-16 07:52:26 +0530

    sql

---
+++ Demo2.sql
@@ -0,0 +1,22 @@
+show databases;
+create database DatabaseEngineers;
+use DatabaseEngineers;
+show databases;
+drop database datbaseengineers;
+commit;
+show databases;
+SELECT CURRENT_USER();
+-- backup command need to understand
+create table Persons(
+   personId int,
+   personanme varchar(255),
+   address varchar(255),
+   city varchar(255)
+);
+commit;
+desc Persons;
+create table PersonsBackup as select * from Persons;
+commit;
+desc PersonsBackup;
+
+
